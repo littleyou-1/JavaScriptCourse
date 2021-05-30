929a4ee6009c39506b6e1ea9a0b431067af8f943
Ivan <ivanovyz25@gmail.com>
2021-05-30 14:42:49 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,67 @@
+"use sctrict";
+
+//число
+let number = 4.6;
+
+console.log(-4/0);
+console.log('string'/9);
+
+//строка
+const persone = 'Alex';
+
+const bool = false;
+
+//console.log(something);
+
+let und;
+console.log(und);
+
+const obj ={
+    name: "Jone",
+    age:25,
+    isMarried: false
+};
+
+//console.log(obj.name);
+//console.log(obj['name']);
+
+
+
+
+let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmb', {}, []];
+
+console.log(arr[1]);
+
+
+//7 urok
+
+//alert(hello);
+
+//const result = confirm("Are you here?");
+//console.log(result);
+
+//const answer = prompt("Вам є 18?", "18");
+//console.log(typeof(answer));
+
+const answers = [];
+
+//answers[0] = prompt('Как ваше имя ', '');
+//answers[1] = prompt('Как ваше фамилия ', '');
+//answers[2] = prompt('Как ваше отчество? ', '');
+
+
+
+///document.write(answers);
+console.log(typeof(answers));
+
+let incr = 10,
+    decr = 10;
+
+++incr;
+--decr;
+
+const isCheked = true,
+        isClose= false;
+
+     console.log(isCheked || isClose);   
+
